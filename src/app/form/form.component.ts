5c647f20b865b951d4f1d7f9efd354a5cca5a850
Flexsin <flexsin@Flexsins-iMac-2.local>
2018-02-16 19:15:39 +0530

    SecondCommit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import { Employee } from '../Model/employee.model';
 import { empty } from 'rxjs/Observer';
+import { indexDebugNode } from '@angular/core/src/debug/debug_node';
 
 @Component({
   selector: 'app-form',
@@ -9,14 +10,21 @@ import { empty } from 'rxjs/Observer';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+
+
 Name: string;
 Email: string;
 Contact: number;
-  constructor() { }
-emp: Employee;
-Add(form: NgForm) {
 
-  this.emp = form.value;
+  constructor() { }
+
+
+
+emp = [];
+Add(form: NgForm) {
+  console.log(form.value);
+this.emp.push(form.value);
+
   console.log(this.emp);
   this.Name = '';
   this.Email = '';
