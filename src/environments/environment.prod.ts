4596c29c6e77c7c3c33ecc199eef21b0f942237f
Sohbis <Soham.biswas.19@gmail.com>
2018-08-11 03:46:28 +0530

    fb3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+ production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCK5jijJA3sq_h3V4HbpsC_oE4dK0HaWYI',
+    authDomain: 'test-89d9b.firebaseapp.com',
+    databaseURL: 'https://test-89d9b.firebaseio.com',
+    projectId: 'test-89d9b',
+    storageBucket: 'test-89d9b.appspot.com',
+    messagingSenderId: '796930900655'
+
+  }
 };
